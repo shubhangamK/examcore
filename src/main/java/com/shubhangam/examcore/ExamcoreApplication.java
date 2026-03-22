0077cac89609df92a2edbcf395a6f6671b3514ec
Shubhangam <shubhangamk1@gmail.com>
2026-03-22 12:39:54 +0530

    Initial project setup - Spring Boot 3.5.12 + MongoDB + Security + Lombok

---
+++ src/main/java/com/shubhangam/examcore/ExamcoreApplication.java
@@ -0,0 +1,13 @@
+package com.shubhangam.examcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamcoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamcoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shubhangam/examcore/ExamcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhangam.examcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamcoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
