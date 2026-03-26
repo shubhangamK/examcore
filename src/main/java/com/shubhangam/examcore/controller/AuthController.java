44dfcbbfed96e4115deadaaf1959f065841e833e
Shubhangam <shubhangamk1@gmail.com>
2026-03-26 06:24:59 +0530

    adding the login api and the register api

--- src/main/java/com/shubhangam/examcore/controller/AuthController.java
+++ src/main/java/com/shubhangam/examcore/controller/AuthController.java
@@ -4,7 +4,11 @@ import com.shubhangam.examcore.dto.request.LoginRequest;
 import com.shubhangam.examcore.dto.request.RegisterRequest;
 import com.shubhangam.examcore.dto.response.AuthResponse;
 import com.shubhangam.examcore.service.AuthService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -17,12 +21,15 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String register(@RequestBody RegisterRequest request){
-       return authService.register(request);
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request){
+        String message =  authService.register(request);
+        return ResponseEntity.status(201).body(message);
     }
     @PostMapping("/login")
-    public AuthResponse login(@RequestBody LoginRequest request){
-        return authService.login(request);
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
+
+        AuthResponse message = authService.login(request);
+        return ResponseEntity.ok(message);
     }
 
 
